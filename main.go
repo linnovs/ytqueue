9273103b5ca296c9ceb0e12a4d78cf02f8a21d34
Linnovs Liu <no-reply@linnovs.com>
2025-12-23 12:47:50 +0800

    feat(logging): use slog with state home log file

--- main.go
+++ main.go
@@ -1,15 +1,38 @@
 package main
 
 import (
-	"log"
+	"log/slog"
+	"os"
+	"path/filepath"
 
+	"github.com/adrg/xdg"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
+const fileperm = 0o600
+
 func main() {
+	logFile, err := xdg.StateFile("ytqueue/ytqueue.log")
+	if err != nil {
+		slog.Error("unable to get log file path", slog.String("error", err.Error()))
+		os.Exit(1)
+	}
+
+	logFile = filepath.Clean(logFile)
+
+	out, err := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, fileperm)
+	if err != nil {
+		slog.Error("unable to open log file", slog.String("error", err.Error()))
+		os.Exit(1)
+	}
+
+	handler := slog.NewTextHandler(out, &slog.HandlerOptions{Level: slog.LevelDebug})
+	slog.SetDefault(slog.New(handler))
+
 	p := tea.NewProgram(newModel(), tea.WithAltScreen())
 
 	if _, err := p.Run(); err != nil {
-		log.Fatal(err)
+		slog.Error("application crashed", slog.String("error", err.Error()))
+		os.Exit(1)
 	}
 }
