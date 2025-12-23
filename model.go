0e8cd79d1e519b7d7fefc33f706a018fa4937e8e
Linnovs Liu <no-reply@linnovs.com>
2025-12-23 20:33:16 +0800

    feat(ui): add topbar with version

--- model.go
+++ model.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log/slog"
 	"net/url"
 
 	"github.com/charmbracelet/bubbles/help"
@@ -11,61 +10,29 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
-type errorMsg struct {
-	err error
-}
-
-func errorCmd(err error) tea.Cmd {
-	return func() tea.Msg {
-		return errorMsg{err}
-	}
-}
-
 type appModel struct {
-	height       int
-	width        int
-	keymap       keymap
-	help         help.Model
-	urlPrompt    *urlPrompt
-	topbarStyle  lipgloss.Style
-	headerStyle  lipgloss.Style
-	versionStyle lipgloss.Style
-	errorStyle   lipgloss.Style
-	err          error
+	keymap     keymap
+	help       help.Model
+	urlPrompt  *urlPrompt
+	topbar     topbar
+	errorStyle lipgloss.Style
+	err        error
 }
 
 func newModel() appModel {
-	prompt := newURLPrompt()
-	prompt.Focus()
-
-	topbarStyle := lipgloss.NewStyle().
-		Foreground(lipgloss.AdaptiveColor{Light: "250", Dark: "255"}).
-		Background(lipgloss.AdaptiveColor{Light: "232", Dark: "0"})
-	headerStyle := lipgloss.NewStyle().
-		Background(lipgloss.AdaptiveColor{Light: "98", Dark: "91"}).
-		Padding(0, 1)
-	versionStyle := lipgloss.NewStyle().
-		Background(lipgloss.AdaptiveColor{Light: "240", Dark: "238"}).
-		Padding(0, 1)
-	errorStyle := lipgloss.NewStyle().Foreground(lipgloss.AdaptiveColor{Light: "196", Dark: "124"})
-
 	return appModel{
-		keymap:       newKeymap(),
-		help:         help.New(),
-		urlPrompt:    prompt,
-		topbarStyle:  topbarStyle,
-		headerStyle:  headerStyle,
-		versionStyle: versionStyle,
-		errorStyle:   errorStyle,
+		keymap:     newKeymap(),
+		help:       help.New(),
+		urlPrompt:  newURLPrompt(),
+		topbar:     newTopbar(),
+		errorStyle: newErrorStyle(),
 	}
 }
 
-// Init implements [tea.Model].
 func (m appModel) Init() tea.Cmd {
 	return nil
 }
 
-// Update implements [tea.Model].
 func (m appModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmds []tea.Cmd
 
@@ -89,35 +56,24 @@ func (m appModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			}
 
 			cmds = append(cmds, errorCmd(nil))
+			cmds = append(cmds, downloadCmd(filmUrl.String()))
 
-			slog.Info("current URL", slog.String("url", filmUrl.String()))
 			m.urlPrompt.Reset()
 		}
-	case tea.WindowSizeMsg:
-		m.height = msg.Height
-		m.width = msg.Width
-		m.urlPrompt.setWidth(msg.Width)
 	case errorMsg:
 		m.err = msg.err
 	}
 
 	var cmd tea.Cmd
 	m.urlPrompt, cmd = m.urlPrompt.Update(msg)
-
+	cmds = append(cmds, cmd)
+	m.topbar, cmd = m.topbar.Update(msg)
 	cmds = append(cmds, cmd)
 
 	return m, tea.Batch(cmds...)
 }
 
-// View implements [tea.Model].
 func (m appModel) View() string {
-	header := m.headerStyle.Render("Short Film Downloader")
-	helpText := m.topbarStyle.Render("Press F1 for help")
-	version := m.versionStyle.Width(m.width - lipgloss.Width(header) - lipgloss.Width(helpText)).
-		Render(version)
-	topbar := m.topbarStyle.Width(m.width).
-		Render(lipgloss.JoinHorizontal(lipgloss.Top, header, version, helpText))
-
 	var errorSection string
 	if m.err != nil {
 		errorSection = m.errorStyle.Render(fmt.Sprintf("Error: %s", m.err))
@@ -125,7 +81,7 @@ func (m appModel) View() string {
 
 	return lipgloss.JoinVertical(
 		lipgloss.Center,
-		topbar,
+		m.topbar.View(),
 		m.urlPrompt.View(),
 		m.help.View(m.keymap),
 		errorSection,
---
+++ topbar.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+type topbar struct {
+	titleStyle    lipgloss.Style
+	versionStyle  lipgloss.Style
+	helpTextStyle lipgloss.Style
+	topbarStyle   lipgloss.Style
+
+	width int
+}
+
+func newTopbar() topbar {
+	titleStyle := lipgloss.NewStyle().
+		Background(lipgloss.Color("91")).
+		Padding(0, 1).
+		SetString("Short Film Downloader")
+	versionStyle := lipgloss.NewStyle().
+		Background(lipgloss.Color("177")).
+		Padding(0, 1)
+	topbarStyle := lipgloss.NewStyle().
+		Foreground(lipgloss.Color("255")).
+		Background(lipgloss.Color("0"))
+	helpTextStyle := topbarStyle.Padding(0, 1).SetString("Press F1 for help")
+
+	return topbar{titleStyle, versionStyle, helpTextStyle, topbarStyle, 0}
+}
+
+func (t topbar) Init() tea.Cmd { return nil }
+
+func (t topbar) Update(msg tea.Msg) (topbar, tea.Cmd) {
+	if msg, ok := msg.(tea.WindowSizeMsg); ok {
+		t.width = msg.Width
+	}
+
+	return t, nil
+}
+
+func (t topbar) View() string {
+	w := lipgloss.Width
+
+	title := t.titleStyle.Render()
+	version := t.versionStyle.Render(version)
+	helpText := t.helpTextStyle.Render()
+	spacing := t.topbarStyle.Width(t.width - w(title) - w(version) - w(helpText)).Render()
+
+	return t.topbarStyle.Width(t.width).
+		Render(lipgloss.JoinHorizontal(lipgloss.Top, title, version, spacing, helpText))
+}
