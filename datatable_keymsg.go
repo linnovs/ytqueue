d50d0174f636480a9f2d1b966bca3949dca18542
Linnovs Liu <no-reply@linnovs.com>
2025-12-28 02:17:41 +0800

    refactor(datatable): move keymsg handler into its own file

---
+++ datatable_keymsg.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"github.com/charmbracelet/bubbles/key"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+func (d *datatable) keyMsgHandler(msg tea.KeyMsg) tea.Cmd {
+	var cmd tea.Cmd
+
+	if !d.isFocused {
+		return cmd
+	}
+
+	switch {
+	case key.Matches(msg, key.NewBinding(key.WithKeys("esc"))):
+		d.deleteConfirm = false
+	case key.Matches(msg, d.keymap.lineUp):
+		d.lineUp(1)
+	case key.Matches(msg, d.keymap.lineDown):
+		d.lineDown(1)
+	case key.Matches(msg, d.keymap.pageUp):
+		d.pageUp()
+	case key.Matches(msg, d.keymap.pageDown):
+		d.pageDown()
+	case key.Matches(msg, d.keymap.halfPageUp):
+		d.halfPageUp()
+	case key.Matches(msg, d.keymap.halfPageDown):
+		d.halfPageDown()
+	case key.Matches(msg, d.keymap.gotoTop):
+		d.gotoTop()
+	case key.Matches(msg, d.keymap.gotoBottom):
+		d.gotoBottom()
+	case key.Matches(msg, d.keymap.moveUp):
+		d.moveUp()
+	case key.Matches(msg, d.keymap.moveDown):
+		d.moveDown()
+	case key.Matches(msg, d.keymap.deleteRow):
+		if d.deleteConfirm {
+			cmd = d.deleteCurrentRow(d.cursor)
+		}
+
+		d.deleteConfirm = !d.deleteConfirm
+	}
+
+	return cmd
+}
--- datatable_navigation.go
+++ datatable_navigation.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	"github.com/charmbracelet/bubbles/key"
-	tea "github.com/charmbracelet/bubbletea"
-)
-
 func (d *datatable) scrollUp() {
 	if d.cursor < d.viewport.YOffset {
 		d.viewport.SetYOffset(d.cursor)
@@ -71,45 +66,4 @@ func (d *datatable) moveDown() {
 	d.cursor = lowerIdx
 	d.scrollDown()
 	d.updateViewport()
-}
-
-func (d *datatable) keyMsgHandler(msg tea.KeyMsg) tea.Cmd {
-	var cmd tea.Cmd
-
-	if !d.isFocused {
-		return cmd
-	}
-
-	switch {
-	case key.Matches(msg, key.NewBinding(key.WithKeys("esc"))):
-		d.deleteConfirm = false
-	case key.Matches(msg, d.keymap.lineUp):
-		d.lineUp(1)
-	case key.Matches(msg, d.keymap.lineDown):
-		d.lineDown(1)
-	case key.Matches(msg, d.keymap.pageUp):
-		d.pageUp()
-	case key.Matches(msg, d.keymap.pageDown):
-		d.pageDown()
-	case key.Matches(msg, d.keymap.halfPageUp):
-		d.halfPageUp()
-	case key.Matches(msg, d.keymap.halfPageDown):
-		d.halfPageDown()
-	case key.Matches(msg, d.keymap.gotoTop):
-		d.gotoTop()
-	case key.Matches(msg, d.keymap.gotoBottom):
-		d.gotoBottom()
-	case key.Matches(msg, d.keymap.moveUp):
-		d.moveUp()
-	case key.Matches(msg, d.keymap.moveDown):
-		d.moveDown()
-	case key.Matches(msg, d.keymap.deleteRow):
-		if d.deleteConfirm {
-			cmd = d.deleteCurrentRow(d.cursor)
-		}
-
-		d.deleteConfirm = !d.deleteConfirm
-	}
-
-	return cmd
 }
