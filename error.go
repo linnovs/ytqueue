6ba6820d59fe5328973e4ee307a21dc6ce9ce460
Linnovs Liu <no-reply@linnovs.com>
2025-12-27 16:45:03 +0800

    feat(error): add reset error message cmd

--- error.go
+++ error.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"time"
+
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
@@ -9,10 +11,24 @@ type errorMsg struct {
 	err error
 }
 
+const defaultErrorTimeout = time.Second * 3
+
 func errorCmd(err error) tea.Cmd {
 	return func() tea.Msg {
 		return errorMsg{err}
 	}
+}
+
+func resetErrorMsgCmd(timeoutArg ...time.Duration) tea.Cmd {
+	timeout := defaultErrorTimeout
+
+	if len(timeoutArg) >= 1 {
+		timeout = timeoutArg[0]
+	}
+
+	return tea.Tick(timeout, func(t time.Time) tea.Msg {
+		return errorMsg{nil}
+	})
 }
 
 func newErrorStyle() lipgloss.Style {
