bc81d97283ab005b5a7f36adcec99bc7a35ca9c8
Linnovs Liu <no-reply@linnovs.com>
2025-12-31 00:31:30 +0800

    refactor(datatable): separate datatable render into another file

--- datatable.go
+++ datatable.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"slices"
-	"strings"
 	"sync"
 
 	"github.com/charmbracelet/bubbles/progress"
@@ -10,7 +8,6 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/linnovs/ytqueue/database"
-	"github.com/mattn/go-runewidth"
 )
 
 func clamp(n, low, high int) int {
@@ -176,109 +173,4 @@ func (d *datatable) setHeight(height int) {
 	height -= d.styles.GetVerticalFrameSize()
 	d.styles = d.styles.Height(height)
 	d.updateViewport()
-}
-
-func (d *datatable) renderHeader() string {
-	var s strings.Builder
-
-	for _, col := range d.columns {
-		style := lipgloss.NewStyle().Width(d.widths[col]).Padding(0, dtCellPadding)
-		s.WriteString(style.Render(string(col)))
-	}
-
-	return lipgloss.NewStyle().
-		Border(lipgloss.NormalBorder(), false, false, true).
-		Render(s.String())
-}
-
-// this function assumes the caller holds the rowMu Rlock.
-func (d *datatable) renderRow(r int) string {
-	d.cursorMu.RLock()
-	defer d.cursorMu.RUnlock()
-
-	var s strings.Builder
-
-	if d.deleteConfirm && r == d.cursor {
-		return lipgloss.NewStyle().
-			Width(d.width).
-			Align(lipgloss.Center).
-			Foreground(lipgloss.Color("0")).
-			Background(lipgloss.Color("9")).
-			Render("Delete this row? (press 'x' again to confirm, 'esc' to cancel)")
-	}
-
-	isPlaying := d.player.getCurrentlyPlayingId() == d.rows[r][colID]
-	rowStyle := lipgloss.NewStyle()
-
-	if r == d.cursor {
-		rowStyle = d.selectedRowStyle
-
-		if d.isFocused {
-			rowStyle = rowStyle.Background(d.focusedBGColor)
-		}
-	}
-
-	for _, colKey := range d.columns {
-		style := lipgloss.NewStyle().Width(d.widths[colKey]).Padding(0, dtCellPadding)
-		cellWidth := d.widths[colKey] - style.GetHorizontalFrameSize()
-		colValue := d.rows[r][colKey]
-
-		switch colKey {
-		case colName:
-			if d.nameTruncateLeft > 0 && r == d.cursor {
-				colValue = runewidth.TruncateLeft(colValue, d.nameTruncateLeft, "…")
-			}
-		case colWatched:
-			style = style.Align(lipgloss.Center)
-
-			if isPlaying {
-				colValue = "PLAYING"
-				rowStyle = rowStyle.Bold(true).Background(lipgloss.Color("34"))
-			}
-		case colURL:
-			if isPlaying {
-				fullWidth := d.widths[colURL] + d.widths[colLocation]
-				p := d.player.renderPlayProgress(cellWidth + d.widths[colLocation])
-				s.WriteString(style.Width(fullWidth).Render(p))
-
-				continue
-			}
-		case colLocation:
-			if isPlaying {
-				continue
-			}
-
-			colValue = shortenPath(colValue)
-		}
-
-		colValue = runewidth.Truncate(colValue, cellWidth, "…")
-		s.WriteString(rowStyle.Render(style.Render(colValue)))
-	}
-
-	return s.String()
-}
-
-func (d *datatable) View() string {
-	playingRow := ""
-	header := d.renderHeader()
-	d.viewport.Height = d.styles.GetHeight() - lipgloss.Height(header)
-
-	if d.player.isPlaying() {
-		d.rowMu.RLock()
-		idx := slices.IndexFunc(d.rows, playingIDIndexFunc(d.player.getCurrentlyPlayingId()))
-
-		if idx >= d.viewport.YOffset+d.viewport.Height {
-			d.viewport.Height--
-			playingRow = d.renderRow(idx)
-		}
-		d.rowMu.RUnlock()
-	}
-
-	content := lipgloss.JoinVertical(lipgloss.Top, header, d.viewport.View())
-
-	if playingRow != "" {
-		content = lipgloss.JoinVertical(lipgloss.Top, content, playingRow)
-	}
-
-	return d.styles.Width(d.width).Render(content)
 }
---
+++ datatable_render.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"slices"
+	"strings"
+
+	"github.com/charmbracelet/lipgloss"
+	"github.com/mattn/go-runewidth"
+)
+
+func (d *datatable) renderHeader() string {
+	var s strings.Builder
+
+	for _, col := range d.columns {
+		style := lipgloss.NewStyle().Width(d.widths[col]).Padding(0, dtCellPadding)
+		s.WriteString(style.Render(string(col)))
+	}
+
+	return lipgloss.NewStyle().
+		Border(lipgloss.NormalBorder(), false, false, true).
+		Render(s.String())
+}
+
+// this function assumes the caller holds the rowMu Rlock.
+func (d *datatable) renderRow(r int) string {
+	d.cursorMu.RLock()
+	defer d.cursorMu.RUnlock()
+
+	var s strings.Builder
+
+	if d.deleteConfirm && r == d.cursor {
+		return lipgloss.NewStyle().
+			Width(d.width).
+			Align(lipgloss.Center).
+			Foreground(lipgloss.Color("0")).
+			Background(lipgloss.Color("9")).
+			Render("Delete this row? (press 'x' again to confirm, 'esc' to cancel)")
+	}
+
+	isPlaying := d.player.getCurrentlyPlayingId() == d.rows[r][colID]
+	rowStyle := lipgloss.NewStyle()
+
+	if r == d.cursor {
+		rowStyle = d.selectedRowStyle
+
+		if d.isFocused {
+			rowStyle = rowStyle.Background(d.focusedBGColor)
+		}
+	}
+
+	for _, colKey := range d.columns {
+		style := lipgloss.NewStyle().Width(d.widths[colKey]).Padding(0, dtCellPadding)
+		cellWidth := d.widths[colKey] - style.GetHorizontalFrameSize()
+		colValue := d.rows[r][colKey]
+
+		switch colKey {
+		case colName:
+			if d.nameTruncateLeft > 0 && r == d.cursor {
+				colValue = runewidth.TruncateLeft(colValue, d.nameTruncateLeft, "…")
+			}
+		case colWatched:
+			style = style.Align(lipgloss.Center)
+
+			if isPlaying {
+				colValue = "PLAYING"
+				rowStyle = rowStyle.Bold(true).Background(lipgloss.Color("34"))
+			}
+		case colURL:
+			if isPlaying {
+				fullWidth := d.widths[colURL] + d.widths[colLocation]
+				p := d.player.renderPlayProgress(cellWidth + d.widths[colLocation])
+				s.WriteString(style.Width(fullWidth).Render(p))
+
+				continue
+			}
+		case colLocation:
+			if isPlaying {
+				continue
+			}
+
+			colValue = shortenPath(colValue)
+		}
+
+		colValue = runewidth.Truncate(colValue, cellWidth, "…")
+		s.WriteString(rowStyle.Render(style.Render(colValue)))
+	}
+
+	return s.String()
+}
+
+func (d *datatable) View() string {
+	playingRow := ""
+	header := d.renderHeader()
+	d.viewport.Height = d.styles.GetHeight() - lipgloss.Height(header)
+
+	if d.player.isPlaying() {
+		d.rowMu.RLock()
+		idx := slices.IndexFunc(d.rows, playingIDIndexFunc(d.player.getCurrentlyPlayingId()))
+
+		if idx >= d.viewport.YOffset+d.viewport.Height {
+			d.viewport.Height--
+			playingRow = d.renderRow(idx)
+		}
+		d.rowMu.RUnlock()
+	}
+
+	content := lipgloss.JoinVertical(lipgloss.Top, header, d.viewport.View())
+
+	if playingRow != "" {
+		content = lipgloss.JoinVertical(lipgloss.Top, content, playingRow)
+	}
+
+	return d.styles.Width(d.width).Render(content)
+}
